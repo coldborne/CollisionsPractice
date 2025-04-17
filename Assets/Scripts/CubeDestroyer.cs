61606b284eeef6ea6f5e8d8004f17554c39c6c3d
coldborne <coldborneVV@gmail.com>
2025-04-17 12:24:20 +0300

    Убрал СОП из класса CubeDestroyer, теперь к данному объекту надо обращаться вручную и "просить" его удалить нужный куб

--- Assets/Scripts/CubeDestroyer.cs
+++ Assets/Scripts/CubeDestroyer.cs
@@ -1,25 +1,9 @@
-using System;
 using UnityEngine;
 
 public class CubeDestroyer : MonoBehaviour
 {
-    [SerializeField] private HitHandler _hitHandler;
-
-    public event Predicate<Cube> Destroyed;
-
-    private void OnEnable()
-    {
-        _hitHandler.Hit += Destroy;
-    }
-
-    private void OnDisable()
-    {
-        _hitHandler.Hit -= Destroy;
-    }
-
-    private void Destroy(Cube cube)
+    public void Destroy(Cube cube)
     {
         Destroy(cube.gameObject);
-        Destroyed?.Invoke(cube);
     }
 }
